859b6248bb0b5bcca0b440334ce8ddbe0c26f83f
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-07-21 01:12:33 +0500

    Add files via upload

---
+++ Q20/app.ts
@@ -0,0 +1,16 @@
+
+
+
+//                              QUESTION : 20
+
+
+//Think of something you could store in a array. For example, you could make
+// a list of mountains, rivers, countries, cities, languages, or anything else you’d like.
+// Write a program that creates a list containing these items
+
+
+let muslim_countries : string [] = ['Pakistan', 'Saudia Arabia', 'Turkey', 'Iran','Iraq', 'Palestine'];
+
+console.log(muslim_countries);
+
+
