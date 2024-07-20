a474e6413eab0a0884fdb812ffd4877c9f863d56
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-07-21 01:19:26 +0500

    Add files via upload

---
+++ Q.23/app.ts
@@ -0,0 +1,103 @@
+
+
+//                                     QUESTION : 23
+
+
+
+
+//Conditional Tests: Write a series of conditional tests.
+
+//Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+//let car = 'subaru';
+
+//console.log("Is car == 'subaru'? I predict True.")
+
+//console.log(car == 'subaru')
+
+//Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+//Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+
+// Write a series of conditional tests.
+
+
+let car : string = 'subaru' ; 
+
+// test # 1
+
+console.log("Is car == 'subaru'? I predict True.");
+
+console.log(car == 'subaru');
+
+//test # 2
+
+console.log("Is car  === 'subaru'? I predict True.");
+
+console.log(car === 'subaru');
+
+//test # 3
+
+console.log("Is car != 'Honda'? ? I predict True.");
+
+console.log(car != 'Honda');
+
+// test # 4
+
+
+console.log("Is car !== 'Ford'? ? I predict True.");
+
+console.log(car !== 'Ford');
+
+
+// test # 5
+
+
+console.log("Is car  upper case == 'SUBARU'? ? I predict True.");
+
+console.log(car.toUpperCase() == 'SUBARU');
+
+// test # 6
+
+console.log("Is car  == 'SUBARU'? ? I predict FALSE.");
+
+console.log(car == 'SUBARU');
+
+
+// test # 7
+
+console.log("Is car = == 'SUBARU'? ? I predict FALSE.");
+
+console.log(car === 'SUBARU');
+
+
+// test # 8
+
+console.log("Is car = == 'Train'? ? I predict FALSE.");
+
+console.log(car === 'Train');
+
+
+// test # 9
+
+console.log("Is car = == 'Bus'? ? I predict FALSE.");
+
+console.log(car === 'Bus');
+
+
+
+// test # 10
+
+console.log("Is car = == 'Cycle'? ? I predict FALSE.");
+
+console.log(car === 'Cycle');
+
+
+
+// ( single equal = )  kisi bhi value ko assign krny k liy use hota hy.
+// ( double equal == ) kisi bhi value ko check krny k liy use hota hy
+// ( triple equal ===) kisi bhi value ko us k data type k sath strictly check krny k liy use hota hy.
+// ( sign of exclaimation (!=)  ye dono sign mila kr not equal k liy use hoty hy.)
+
+
