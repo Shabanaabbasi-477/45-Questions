08234a538be6ac7bbac1e4889610440af2928468
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-07-21 00:23:10 +0500

    Add files via upload

---
+++ Q10. Adding Comments/program1.js
@@ -0,0 +1,14 @@
+"use strict";
+//                      QUESTION : 10
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+//If you don’t have anything specific to write because your programs are too simple at this point,
+// just add your name and the current date at the top of each program file.
+// Then write one sentence describing what the program does.
+// Program Written By : Shabana Abbasi
+// Date               : 01-03-2024
+// This program will print hello world
+console.log("Hello World");
+// This program is written by : Shabana Abbasi
+//      Date                  : 01-03-2024
+// This program will add two digits 
+console.log(4 + 4);
