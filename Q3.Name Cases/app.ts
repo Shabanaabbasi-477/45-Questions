acbc50f13e31901a16583a9f40e868e42b317c2a
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-07-20 23:59:04 +0500

    Add files via upload

---
+++ Q3.Name Cases/app.ts
@@ -0,0 +1,12 @@
+
+//              QUESTION  : 3
+
+// Name Cases: Store a person’s name in a variable, and then print that person’s name
+//in lowercase, uppercase, and titlecase.
+
+
+
+let person_name: string = "Shabana Abbasi";
+console.log("Lowercase :",person_name.toLowerCase());
+console.log("Uppercase :",person_name.toUpperCase());
+console.log("Titlecase :",person_name.charAt(0).toUpperCase() + person_name.slice(1));
