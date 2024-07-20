fe8b1218f1cc21f10c25d05944deb2c73a609a66
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-07-21 01:11:18 +0500

    Add files via upload

---
+++ Q19/app.js
@@ -0,0 +1,6 @@
+"use strict";
+//                                      QUESTION : 19
+//  Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+// print a message indicating the number of people you are inviting to dinner.
+let guest_list = ["Iqra", "Eesha", "Huma", "Kainat", "Hira"];
+console.log(`\n Total number of guests are : ${guest_list.length}`);
---
+++ Q19/app.ts
@@ -0,0 +1,14 @@
+
+
+
+
+//                                      QUESTION : 19
+
+
+//  Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+// print a message indicating the number of people you are inviting to dinner.
+
+
+let guest_list : string[] = ["Iqra", "Eesha","Huma", "Kainat", "Hira"];
+
+console.log(`\n Total number of guests are : ${guest_list.length}`);
