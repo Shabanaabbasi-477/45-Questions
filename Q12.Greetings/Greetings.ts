e1a905870574d90fab829ce7ef28939b3c52fecd
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-07-21 01:01:00 +0500

    Add files via upload

---
+++ Q12.Greetings/Greetings.ts
@@ -0,0 +1,20 @@
+
+
+
+//                                          QUESTION : 12
+
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+// print a message to them. The text of each message should be the same,
+// but each message should be personalized with the person’s name.
+
+
+
+
+let names : string[] = ["Saba","Zoya","Anum","Shani","Esha"];
+
+const message :string = "Wellcome to Typescript class. ";
+
+for (let i=0; i<names.length; i++){
+
+console.log(`\n ${names[i]} , ${message}`)
+}
