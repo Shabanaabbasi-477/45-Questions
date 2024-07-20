1a99040cdf7aecd7d2fca19ea9809561c7d9571b
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-07-21 01:17:58 +0500

    Add files via upload

---
+++ Q22/app.js
@@ -0,0 +1,10 @@
+"use strict";
+//                          QUESTION : 22
+//Intentional Error: If you haven’t received an array index error in one of your programs yet,
+// try to make one happen. Change an index in one of your programs to produce an index error. 
+//Make sure you correct the error before closing the program.
+let actress_Array = ["Saba Qamar", "Ushna Shah", "Zara Noor", "Bushra Ansari", "Sajal Ali"];
+// error in array index number
+console.log(actress_Array[8]);
+// corrected array error
+console.log(actress_Array);
