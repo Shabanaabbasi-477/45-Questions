da0bf94c6ab1531831b0240030e6574cd761b308
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-07-21 00:03:38 +0500

    Add files via upload

---
+++ Q4.Famous_Quote/app.ts
@@ -0,0 +1,9 @@
+
+//                      QUESTION : 4
+
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
+console.log('Imam Ali once said,"The World Can Not Defeat You Untill You Accept The Defeat."');
