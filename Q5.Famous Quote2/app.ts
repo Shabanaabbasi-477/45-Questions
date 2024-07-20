2986b59a0aa4b3a622700acb1d4a931f30cbce00
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-07-21 00:13:06 +0500

    Add files via upload

---
+++ Q5.Famous Quote2/app.ts
@@ -0,0 +1,15 @@
+ 
+ //                 QUESTION : 5
+
+
+ 
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+
+//Then compose your message and store it in a new variable called message. Print your message.
+
+
+
+
+ let famouse_person :string = 'Imam Ali';
+ let message :string = '"People Are Asleep. When They Die, They Will Wake Up."';
+ console.log(`${famouse_person}  once said, ${message}`);
