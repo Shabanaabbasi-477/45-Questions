4a49d283391217b276321e9a36b5c1dba210a135
Shabanaabbasi-477 <161618255+Shabanaabbasi-477@users.noreply.github.com>
2024-07-21 00:18:56 +0500

    Add files via upload

---
+++ Q8.Number_8/Number_8.ts
@@ -0,0 +1,18 @@
+
+ //                       QUESTION :8
+
+
+
+// You should create four lines that look like this: console.log(5 + 3) Your output should simply be
+// four lines with the number 8 appearing once on each line.
+
+
+
+
+console.log(4+4);  // addition
+
+console.log(14-6);  //subtraction
+
+console.log(4*2);   //multiplication
+
+console.log(40/5);  //division
